f4d3f1c1ec881f5137e2803b035021b31fa58338
codyowl <codyowl@gmail.com>
2025-04-28 01:15:19 +0000

    utilities initialized with basic skeleton :baby_bottle:

---
+++ utilities/k8sResources.go
@@ -0,0 +1,18 @@
+package utilities
+
+import (
+	"fmt"
+	"strings"
+	"os/exec"
+)
+
+
+// helper function to read and create resources from a directory
+func Createk8sResources(rootDir, namespace string) error {
+	
+}
+
+// helper function to create a namespace if not exists
+func createNamespaceIfNotExist(namespace string) error {
+
+}
