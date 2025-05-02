cc1edb27e53bd87fbbe32bf1e0f4cedf9f791818
codyowl <codyowl@gmail.com>
2025-05-02 00:48:11 +0000

    create namespace if not exists added :construction:

--- utilities/k8sResources.go
+++ utilities/k8sResources.go
@@ -14,5 +14,14 @@ func Createk8sResources(rootDir, namespace string) error {
 
 // helper function to create a namespace if not exists
 func createNamespaceIfNotExist(namespace string) error {
-
+	getNamespacecmd := exec.Command("kubectl", "get", "namespace", namespace)
+	if err := getNamespacecmd.Run(); err != nil {
+		fmt.Printf("Namespace %s not found", namespace)
+		createNamespaceCmd := exec.Command("kubectl", "create", "namespace", namespace)
+		createNamespaceCmd.Stdout = os.Stdout
+		createNamespaceCmd.Stderr = os.Stderr
+		return createNamespaceCmd.Run()
+	}
+	fmt.Printf("Namespace %s found, using existing namespace", namespace)
+	return nil
 }
